991344c64f2758c667744835c7d8b5f75b67477a
Aex <adaex.yang@gmail.com>
2021-11-03 17:56:57 +0000

    improve update.sh

--- update.sh
+++ update.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
+# init
 cd /workspaces
-
 rm -rf vscode seti-ui
 
-git clone https://github.com/jesseweed/seti-ui
-git clone https://github.com/microsoft/vscode
+# clone
+git clone https://github.com/microsoft/vscode --depth 1
+git clone https://github.com/jesseweed/seti-ui --depth 1
 
-cd /workspaces/seti-ui
-npm ci
-npm run prepublishOnly
-
+# init vscode
 cd /workspaces/vscode
 git remote add adaex https://adaex:${AEX_TOKEN}@github.com/adaex/vscode
 git checkout -b update-seti-ui
 yarn
 
+# build seti-ui
+cd /workspaces/seti-ui
+npm ci
+npm run prepublishOnly
+
+# update vscode
 cd /workspaces/vscode/extensions/theme-seti
 npm run update
 
 git add .
 git commit -m "Update to latest seti-ui icon theme"
 git push adaex
+
+# open for preview
+code /workspaces/vscode/extensions/theme-seti
